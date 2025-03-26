7e886c3ebbe4ab7849612e371f1986773b49e90b
Jake Soenneker <jake@soenneker.com>
2025-03-26 11:28:09 -0500

    Initial commit

---
+++ src/NamedEntityRequestOverride.cs
@@ -0,0 +1,20 @@
+﻿using Soenneker.Requests.Entities.Named;
+using Soenneker.Utils.AutoBogus.Context;
+using Soenneker.Utils.AutoBogus.Override;
+
+namespace Soenneker.AutoFaker.Overrides.Requests.Entities.Named;
+
+/// <summary>
+/// An AutoFaker (AutoBogus) override for the NamedEntityRequest object
+/// </summary>
+public class NamedEntityRequestOverride : AutoFakerOverride<NamedEntityRequest>
+{
+    public override bool CanOverride(AutoFakerContext context)
+    {
+        return context.GenerateType.IsSubclassOf(typeof(NamedEntityRequest));
+    }
+
+    public override void Generate(AutoFakerOverrideContext context)
+    {
+    }
+}
---
+++ test/Soenneker.AutoFaker.Overrides.Requests.Entities.Named.Tests/NamedEntityRequestTests.cs
@@ -0,0 +1,13 @@
+﻿using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.AutoFaker.Overrides.Requests.Entities.Named.Tests;
+
+public sealed class NamedEntityRequestTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
